ba1b6f0bf3cfaec3ed40055efb44d0fd5d436849
Roman0407 <105305762+Roman0407@users.noreply.github.com>
2022-10-10 10:51:28 +0000

    Deploying to gh-pages from @ Roman0407/goit-js-hw-09@6252d87645ec58ebf3a789d8f09ea8b708b838ea 🚀

--- 01-color-switcher.ca67ba0f.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=document.querySelector("body");let o;t.addEventListener("click",(function(){t.style.pointerEvents="none",o=setInterval((()=>n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`),1e3)})),e.addEventListener("click",(function(){t.style.pointerEvents="auto",clearInterval(o)}));
-//# sourceMappingURL=01-color-switcher.ca67ba0f.js.map
